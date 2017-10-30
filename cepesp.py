f8715bb1feb0ff12aa9315cd2a013e9136f38b5d
Cepesp-Fgv <fgv.cepesp@gmail.com>
2017-10-29 23:43:04 -0300

    Update cepesp.py

--- cepesp.py
+++ cepesp.py
@@ -14,16 +14,16 @@ def add_filters(request,
                 codigo_municipio=None):
     index = 0
     if (estado != None):
-        request = add_filter(request, "UF", estado, index);
+        request = add_filter(request, "UF", estado, index)
         index += 1
     if (numero_candidato != None):
-        request = add_filter(request, "NUMERO_CANDIDATO", numero_candidato, index);
+        request = add_filter(request, "NUMERO_CANDIDATO", numero_candidato, index)
         index += 1
     if (numero_partido != None):
-        request = add_filter(request, "NUMERO_PARTIDO", numero_partido, index);
+        request = add_filter(request, "NUMERO_PARTIDO", numero_partido, index)
         index += 1
     if (codigo_municipio != None):
-        request = add_filter(request, "CODIGO_MUNICIPIO", codigo_municipio, index);
+        request = add_filter(request, "CODIGO_MUNICIPIO", codigo_municipio, index)
         index += 1
     return request
 
@@ -38,7 +38,7 @@ def votos(cargo=1,
           codigo_municipio=None):
     request = "votos?cargo={}&ano={}&agregacao_politica={}&agregacao_regional={}&format=gzip".format(cargo, ano,
                                                                                                      agregacao_politica,
-                                                                                                     agregacao_regional);
+                                                                                                     agregacao_regional)
     request = add_filters(request, estado, numero_candidato, numero_partido, codigo_municipio)
     filename = hashlib.md5(request).hexdigest() + ".gz"
     response = urllib2.urlopen(baseURL + request)
@@ -56,7 +56,7 @@ def candidatos(cargo=1,
                codigo_municipio=None):
     request = "candidatos?cargo={}&ano={}&agregacao_politica={}&agregacao_regional={}&format=gzip".format(cargo, ano,
                                                                                                           agregacao_politica,
-                                                                                                          agregacao_regional);
+                                                                                                          agregacao_regional)
     request = add_filters(request, estado, numero_candidato, numero_partido, codigo_municipio)
     filename = hashlib.md5(request).hexdigest() + ".gz"
     response = urllib2.urlopen(baseURL + request)
@@ -74,7 +74,7 @@ def legendas(cargo=1,
              codigo_municipio=None):
     request = "legendas?cargo={}&ano={}&agregacao_politica={}&agregacao_regional={}&format=gzip".format(cargo, ano,
                                                                                                         agregacao_politica,
-                                                                                                        agregacao_regional);
+                                                                                                        agregacao_regional)
     filename = hashlib.md5(request).hexdigest() + ".gz"
     request = add_filters(request, estado, numero_candidato, numero_partido, codigo_municipio)
     response = urllib2.urlopen(baseURL + request)
