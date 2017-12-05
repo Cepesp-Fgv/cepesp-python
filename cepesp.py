04297ffa25c003251ac9aa39c4df4e23aa3adc2c
Luiz Gabriel <luizgabriel.info@gmail.com>
2017-12-04 22:29:28 -0300

    Added warning messages

--- cepesp.py
+++ cepesp.py
@@ -3,8 +3,8 @@ import urllib2
 import hashlib
 import os
 
-baseURL = "http://cepesp.io/api/consulta/"
-baseDir = os.path.dirname(__file__)
+BASE_URL = "http://cepesp.io/api/consulta/"
+BASE_DIR = os.path.dirname(__file__)
 
 
 def add_filters(request, estado=None, numero_candidato=None, numero_partido=None, codigo_municipio=None):
@@ -31,16 +31,30 @@ def add_uf_filter(request, estado):
     return request + "&uf_filter={}".format(estado)
 
 
+def warn(function_name):
+    print("[WARN] %s function may return some invalid or incorrect results" % function_name)
+
+
+def query(request):
+    filename = hashlib.md5(request).hexdigest() + ".gz"
+    response = urllib2.urlopen(BASE_URL + request)
+    save_cache(response, filename)
+    return pd.read_csv(os.path.join(BASE_DIR, "cache/" + filename), sep=",", dtype=str)
+
+
+def save_cache(response, filename):
+    file_path = os.path.join(BASE_DIR, "cache/" + filename)
+    with open(file_path, 'wb') as outfile:
+        outfile.write(response.read())
+
+
 def votos(cargo=1, ano=2014, agregacao_politica=1, agregacao_regional=0, estado=None, numero_candidato=None,
           numero_partido=None, codigo_municipio=None):
     request = "votos?cargo={}&ano={}&agregacao_politica={}&agregacao_regional={}&format=gzip".format(cargo, ano,
                                                                                                      agregacao_politica,
                                                                                                      agregacao_regional)
     request = add_filters(request, estado, numero_candidato, numero_partido, codigo_municipio)
-    filename = hashlib.md5(request).hexdigest() + ".gz"
-    response = urllib2.urlopen(baseURL + request)
-    save_cache(response, filename)
-    return pd.read_csv(os.path.join(baseDir, "cache/" + filename), sep=",", dtype=str)
+    return query(request)
 
 
 def candidatos(cargo=1, ano=2014, agregacao_politica=1, agregacao_regional=2, estado=None, numero_candidato=None,
@@ -49,10 +63,7 @@ def candidatos(cargo=1, ano=2014, agregacao_politica=1, agregacao_regional=2, es
                                                                                                           agregacao_politica,
                                                                                                           agregacao_regional)
     request = add_filters(request, estado, numero_candidato, numero_partido, codigo_municipio)
-    filename = hashlib.md5(request).hexdigest() + ".gz"
-    response = urllib2.urlopen(baseURL + request)
-    save_cache(response, filename)
-    return pd.read_csv(os.path.join(baseDir, "cache/" + filename), sep=",", dtype=str)
+    return query(request)
 
 
 def legendas(cargo=1, ano=2014, agregacao_politica=1, agregacao_regional=2, estado=None, numero_candidato=None,
@@ -60,15 +71,13 @@ def legendas(cargo=1, ano=2014, agregacao_politica=1, agregacao_regional=2, esta
     request = "legendas?cargo={}&ano={}&agregacao_politica={}&agregacao_regional={}&format=gzip".format(cargo, ano,
                                                                                                         agregacao_politica,
                                                                                                         agregacao_regional)
-    filename = hashlib.md5(request).hexdigest() + ".gz"
     request = add_filters(request, estado, numero_candidato, numero_partido, codigo_municipio)
-    response = urllib2.urlopen(baseURL + request)
-    save_cache(response, filename)
-    return pd.read_csv(os.path.join(baseDir, "cache/" + filename), sep=",", dtype=str)
+    return query(request)
 
 
 def votos_x_candidatos(cargo=1, ano=2014, agregacao_politica=1, agregacao_regional=2, estado=None,
                        numero_candidato=None):
+    warn("votos_x_candidatos")
     vot = votos(cargo, ano, agregacao_politica, agregacao_regional, estado, numero_candidato)
     cand = candidatos(cargo, ano, agregacao_politica, agregacao_regional, estado, numero_candidato)
     return vot.set_index(["NUMERO_CANDIDATO", "SIGLA_UE", "NUM_TURNO", "ANO_ELEICAO"]).merge(
@@ -77,6 +86,7 @@ def votos_x_candidatos(cargo=1, ano=2014, agregacao_politica=1, agregacao_region
 
 
 def votos_x_legendas(cargo=1, ano=2014, agregacao_politica=1, agregacao_regional=2, estado=None, numero_candidato=None):
+    warn("votos_x_legendas")
     vot = votos(cargo, ano, agregacao_politica, agregacao_regional, estado, numero_candidato)
     leg = legendas(cargo, ano, agregacao_politica, agregacao_regional, estado, numero_candidato)
     leg = leg.rename(columns={"NUMERO_PARTIDO": "NUMERO_CANDIDATO"})
@@ -87,17 +97,12 @@ def votos_x_legendas(cargo=1, ano=2014, agregacao_politica=1, agregacao_regional
 
 def candidato_x_legendas(cargo=1, ano=2014, agregacao_politica=1, agregacao_regional=2, estado=None,
                          numero_candidato=None):
+    warn("candidato_x_legendas")
     leg = legendas(cargo, ano, agregacao_politica, agregacao_regional, estado, numero_candidato)
     cand = candidatos(cargo, ano, agregacao_politica, agregacao_regional, estado, numero_candidato)
     return cand.set_index(["NUMERO_PARTIDO", "SIGLA_UE", "ANO_ELEICAO"]).merge(
         leg.set_index(["NUMERO_PARTIDO", "SIGLA_UE", "ANO_ELEICAO"]), how="left", left_index=True, right_index=True,
         suffixes=["_x", "_y"]).reset_index()
-
-
-def save_cache(response, filename):
-    file_path = os.path.join(baseDir, "cache/" + filename)
-    with open(file_path, 'wb') as outfile:
-        outfile.write(response.read())
 
 
 class CARGO:
