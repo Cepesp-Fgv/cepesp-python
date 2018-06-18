073aa1b84bbd3c0d112b77bc875874d5953b2504
Luiz Gabriel <luizgabriel.info@gmail.com>
2018-06-18 13:46:50 -0300

    Added many changes and fixes to python api

---
+++ cepesp/api.py
@@ -0,0 +1,149 @@
+import urllib as urllib
+
+import pandas as pd
+
+from columns import VOTOS, CANDIDATOS, LEGENDAS
+
+BASE_URL = "http://localhost:5000/api/consulta/"
+
+
+def build_filters(filters):
+    q = ""
+
+    if 'UF' in filters.keys():
+        uf = filters.pop('UF')
+        q += "&uf_filter={}".format(uf)
+
+    i = 0
+    for k, v in filters.items():
+        q += "&columns[{}][name]={}&columns[{}][search][value]={}".format(i, k, i, v)
+        i += 1
+
+    return q
+
+
+def build_columns(columns):
+    q = ""
+    for column in columns:
+        q += "&selected_columns[]={}".format(column)
+
+    return q
+
+
+def query(request, filters, columns):
+    if filters is not None:
+        request += build_filters(filters)
+
+    if columns is not None:
+        request += build_columns(columns)
+
+    response = urllib.urlopen(BASE_URL + request)
+    return pd.read_csv(response, sep=",", dtype=str)
+
+
+def votos(ano, cargo, agregacao_regional, estado=None, numero_candidato=None, filtros=None,
+          colunas=None):
+    request = "votos?cargo={}&ano={}&agregacao_regional={}&format=csv".format(cargo, ano, agregacao_regional)
+    if filtros is None:
+        filtros = dict()
+
+    if estado is not None:
+        filtros['UF'] = estado
+
+    if numero_candidato is not None:
+        filtros['NUMERO_CANDIDATO'] = numero_candidato
+
+    return query(request, filtros, colunas)
+
+
+def candidatos(ano, cargo, filtros=None, colunas=None):
+    request = "candidatos?cargo={}&ano={}&format=csv".format(cargo, ano)
+    return query(request, filtros, colunas)
+
+
+def legendas(ano, cargo, filtros=None, colunas=None):
+    request = "legendas?cargo={}&ano={}&format=csv".format(cargo, ano)
+    return query(request, filtros, colunas)
+
+
+def resolve_conflicts(df, prefer='_x', drop='_y'):
+    columns = df.columns.values.tolist()
+    conflicts = [c for c in columns if c.endswith(prefer)]
+    drops = [c for c in columns if c.endswith(drop)]
+    renames = dict()
+    for c in conflicts:
+        renames[c] = c.replace(prefer, '')
+
+    return df.rename(columns=renames).drop(drops, axis=1)
+
+
+def votos_x_candidatos(ano, cargo, agregacao_regional, estado=None):
+    vot = votos(ano, cargo, agregacao_regional, estado, colunas=VOTOS[agregacao_regional]) \
+        .set_index(["NUMERO_CANDIDATO", "SIGLA_UE", "NUM_TURNO", "ANO_ELEICAO"])
+
+    cand = candidatos(ano, cargo, colunas=CANDIDATOS) \
+        .set_index(["NUMERO_CANDIDATO", "SIGLA_UE", "NUM_TURNO", "ANO_ELEICAO"])
+
+    merged = vot.merge(cand, how="left", left_index=True, right_index=True).reset_index()
+
+    return resolve_conflicts(merged)
+
+
+def votos_x_legendas(ano, cargo, agregacao_regional, estado=None):
+    vot = votos(ano, cargo, agregacao_regional, estado, colunas=VOTOS[agregacao_regional]) \
+        .set_index(["NUMERO_CANDIDATO", "SIGLA_UE", "NUM_TURNO", "ANO_ELEICAO"])
+
+    leg = legendas(ano, cargo, colunas=LEGENDAS) \
+        .rename(columns={"NUMERO_PARTIDO": "NUMERO_CANDIDATO"}) \
+        .set_index(["NUMERO_CANDIDATO", "SIGLA_UE", "NUM_TURNO", "ANO_ELEICAO"])
+
+    merged = vot.merge(leg, how="left", left_index=True, right_index=True).reset_index()
+
+    return resolve_conflicts(merged)
+
+
+def candidato_x_legendas(ano, cargo):
+    leg = legendas(ano, cargo, colunas=LEGENDAS) \
+        .set_index(["NUMERO_PARTIDO", "SIGLA_UE", "ANO_ELEICAO"])
+
+    cand = candidatos(ano, cargo, colunas=CANDIDATOS) \
+        .set_index(["NUMERO_PARTIDO", "SIGLA_UE", "ANO_ELEICAO"])
+
+    merged = cand.merge(leg, how="left", left_index=True, right_index=True).reset_index()
+
+    return resolve_conflicts(merged)
+
+
+def get_elections(cargo):
+    if cargo in [1, 3, 5, 6, 7, 8]:
+        return [2014, 2010, 2006, 2002, 1998]
+    elif cargo in [11, 13]:
+        return [2016, 2012, 2008, 2004, 2000]
+
+
+class CARGO:
+    PRESIDENTE = 1
+    SENADOR = 5
+    GOVERNADOR = 3
+    VEREADOR = 13
+    PREFEITO = 11
+    DEPUTADO_FEDERAL = 6
+    DEPUTADO_ESTADUAL = 7
+    DEPUTADO_DISTRITAL = 8
+
+
+class AGR_REGIONAL:
+    BRASIL = 0
+    UF = 2
+    MUNICIPIO = 6
+    MUNZONA = 7
+    ZONA = 8
+    MACRO = 1
+    MESO = 4
+    MICRO = 5
+
+
+class AGR_POLITICA:
+    CANDIDATO = 1
+    PARTIDO = 2
+    COLIGACAO = 3
---
+++ cepesp/columns.py
@@ -0,0 +1,240 @@
+CANDIDATOS = [
+    'ANO_ELEICAO',
+    'NUM_TURNO',
+    'DESCRICAO_ELEICAO',
+    'SIGLA_UF',
+    'SIGLA_UE',
+    'DESCRICAO_UE',
+    'CODIGO_CARGO',
+    'DESCRICAO_CARGO',
+    'NUMERO_PARTIDO',
+    'SIGLA_PARTIDO',
+    'NOME_CANDIDATO',
+    'NUMERO_CANDIDATO',
+    'CPF_CANDIDATO',
+    'NOME_URNA_CANDIDATO',
+    'COD_SITUACAO_CANDIDATURA',
+    'DES_SITUACAO_CANDIDATURA',
+    'NOME_PARTIDO',
+    'CODIGO_LEGENDA',
+    'SIGLA_LEGENDA',
+    'COMPOSICAO_LEGENDA',
+    'NOME_COLIGACAO',
+    'CODIGO_OCUPACAO',
+    'DESCRICAO_OCUPACAO',
+    'DATA_NASCIMENTO',
+    'NUM_TITULO_ELEITORAL_CANDIDATO',
+    'IDADE_DATA_ELEICAO',
+    'CODIGO_SEXO',
+    'DESCRICAO_SEXO',
+    'COD_GRAU_INSTRUCAO',
+    'DESCRICAO_GRAU_INSTRUCAO',
+    'CODIGO_ESTADO_CIVIL',
+    'DESCRICAO_ESTADO_CIVIL',
+    'CODIGO_COR_RACA',
+    'DESCRICAO_COR_RACA',
+    'CODIGO_NACIONALIDADE',
+    'DESCRICAO_NACIONALIDADE',
+    'SIGLA_UF_NASCIMENTO',
+    'CODIGO_MUNICIPIO_NASCIMENTO',
+    'NOME_MUNICIPIO_NASCIMENTO',
+    'DESPESA_MAX_CAMPANHA',
+    'COD_SIT_TOT_TURNO',
+    'DESC_SIT_TOT_TURNO',
+    'EMAIL_CANDIDATO',
+]
+
+LEGENDAS = [
+    "ANO_ELEICAO",
+    "NUM_TURNO",
+    "DESCRICAO_ELEICAO",
+    "SIGLA_UF",
+    "SIGLA_UE",
+    "CODIGO_CARGO",
+    "DESCRICAO_CARGO",
+    "TIPO_LEGENDA",
+    "NUMERO_PARTIDO",
+    "SIGLA_PARTIDO",
+    "NOME_PARTIDO",
+    "SIGLA_COLIGACAO",
+    "NOME_COLIGACAO",
+    "COMPOSICAO_COLIGACAO",
+    "SEQUENCIAL_COLIGACAO"
+]
+
+VOTOS = {
+
+    # BR
+    0: [
+        "ANO_ELEICAO",
+        "SIGLA_UE",
+        "NUM_TURNO",
+        "DESCRICAO_ELEICAO",
+        "CODIGO_CARGO",
+        "DESCRICAO_CARGO",
+        "NUMERO_CANDIDATO",
+        "QTDE_VOTOS"
+    ],
+
+    # MACRO
+    1: [
+        "ANO_ELEICAO",
+        "CODIGO_MACRO",
+        "NOME_MACRO",
+        "SIGLA_UE",
+        "NUM_TURNO",
+        "DESCRICAO_ELEICAO",
+        "CODIGO_CARGO",
+        "DESCRICAO_CARGO",
+        "NUMERO_CANDIDATO",
+        "QTDE_VOTOS"
+    ],
+
+    # UF
+    2: [
+        "ANO_ELEICAO",
+        "UF",
+        "NOME_UF",
+        "CODIGO_MACRO",
+        "NOME_MACRO",
+        "SIGLA_UE",
+        "NUM_TURNO",
+        "DESCRICAO_ELEICAO",
+        "CODIGO_CARGO",
+        "DESCRICAO_CARGO",
+        "NUMERO_CANDIDATO",
+        "QTDE_VOTOS"
+    ],
+
+    # MESO
+    4: [
+        "ANO_ELEICAO",
+        "CODIGO_MESO",
+        "NOME_MESO",
+        "UF",
+        "NOME_UF",
+        "CODIGO_MACRO",
+        "NOME_MACRO",
+        "SIGLA_UE",
+        "NUM_TURNO",
+        "DESCRICAO_ELEICAO",
+        "CODIGO_CARGO",
+        "DESCRICAO_CARGO",
+        "NUMERO_CANDIDATO",
+        "QTDE_VOTOS"
+    ],
+
+    # MICRO
+    5: [
+        "ANO_ELEICAO",
+        "CODIGO_MICRO",
+        "NOME_MICRO",
+        "CODIGO_MESO",
+        "NOME_MESO",
+        "UF",
+        "NOME_UF",
+        "CODIGO_MACRO",
+        "NOME_MACRO",
+        "SIGLA_UE",
+        "NUM_TURNO",
+        "DESCRICAO_ELEICAO",
+        "CODIGO_CARGO",
+        "DESCRICAO_CARGO",
+        "NUMERO_CANDIDATO",
+        "QTDE_VOTOS"
+    ],
+
+    # MUNICIPIO
+    6: [
+        "ANO_ELEICAO",
+        "COD_MUN_TSE",
+        "COD_MUN_IBGE",
+        "NOME_MUNICIPIO",
+        "CODIGO_MICRO",
+        "NOME_MICRO",
+        "CODIGO_MESO",
+        "NOME_MESO",
+        "UF",
+        "NOME_UF",
+        "CODIGO_MACRO",
+        "NOME_MACRO",
+        "SIGLA_UE",
+        "NUM_TURNO",
+        "DESCRICAO_ELEICAO",
+        "CODIGO_CARGO",
+        "DESCRICAO_CARGO",
+        "NUMERO_CANDIDATO",
+        "QTDE_VOTOS"
+    ],
+
+    # MUNZONA
+    7: [
+        "ANO_ELEICAO",
+        "NUM_ZONA",
+        "COD_MUN_TSE",
+        "COD_MUN_IBGE",
+        "NOME_MUNICIPIO",
+        "CODIGO_MICRO",
+        "NOME_MICRO",
+        "CODIGO_MESO",
+        "NOME_MESO",
+        "UF",
+        "NOME_UF",
+        "CODIGO_MACRO",
+        "NOME_MACRO",
+        "SIGLA_UE",
+        "NUM_TURNO",
+        "DESCRICAO_ELEICAO",
+        "CODIGO_CARGO",
+        "DESCRICAO_CARGO",
+        "NUMERO_CANDIDATO",
+        "QTDE_VOTOS"
+    ],
+
+    # ZONA
+    8: [
+        "ANO_ELEICAO",
+        "NUM_ZONA",
+        "CODIGO_MICRO",
+        "NOME_MICRO",
+        "CODIGO_MESO",
+        "NOME_MESO",
+        "UF",
+        "NOME_UF",
+        "CODIGO_MACRO",
+        "NOME_MACRO",
+        "SIGLA_UE",
+        "NUM_TURNO",
+        "DESCRICAO_ELEICAO",
+        "CODIGO_CARGO",
+        "DESCRICAO_CARGO",
+        "NUMERO_CANDIDATO",
+        "QTDE_VOTOS"
+    ],
+
+    # VOTACAO SECAO
+    9: [
+        "ANO_ELEICAO",
+        "NUM_SECAO",
+        "NUM_ZONA",
+        "CODIGO_MICRO",
+        "NOME_MICRO",
+        "CODIGO_MESO",
+        "NOME_MESO",
+        "UF",
+        "NOME_UF",
+        "CODIGO_MACRO",
+        "NOME_MACRO",
+        "COD_MUN_TSE",
+        "COD_MUN_IBGE",
+        "NOME_MUNICIPIO",
+        "NUM_TURNO",
+        "DESCRICAO_ELEICAO",
+        "SIGLA_UE",
+        "CODIGO_CARGO",
+        "DESCRICAO_CARGO",
+        "NUMERO_CANDIDATO",
+        "QTDE_VOTOS"
+    ]
+
+}
