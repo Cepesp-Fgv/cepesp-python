d5fc9057d4bb08ba1928f21b8b082e56be69f998
Abraao B <abraaobarros3@gmail.com>
2020-01-10 01:21:11 -0300

    Setup pip package to easy install and change to Python 3.7

---
+++ setup.py
@@ -0,0 +1,24 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+     name='electionsBR',  
+     version='0.1',
+     scripts=['electionsBR'] ,
+     author="Abraao Barros",
+     author_email="abraaobarros3@gmail.com",
+     description="A python wrapper to fetch votes from BR elections",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/Cepesp-Fgv/cepesp-python",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
